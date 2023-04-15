207accc96ba0e81465250f535075b4e2ed9438f1
SB1600Python <129617686+SB1600Python@users.noreply.github.com>
2023-04-15 17:59:48 +0300

    Add files via upload

---
+++ Site2/blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Create your models here.
+class Post(models.Model):
+     title = models.CharField(max_length=255)
+     text = models.TextField()
+     image = models.ImageField(upload_to='photos/%Y/%m/%d', null=True, blank=True)
+     category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+     
+
+     def __str__(self):
+          return self.title
+
+class Category(models.Model):
+     name = models.CharField(max_length=255)
+
+     def __str__(self):
+          return self.name
